b1e7553c973b6de394ce42e36616d8dfb1e00291
Andrey Baev <60654928+AbaevB@users.noreply.github.com>
2026-03-12 01:24:24 +0700

    add search

---
+++ src/components/blocks/Search.vue
@@ -0,0 +1,18 @@
+<!-- src/components/blocks/Search.vue -->
+<template>
+    <form action="" method="GET" class="search">
+        <label for="search" class="search__label">
+            <svg class="search__icon" width="24" height="24">
+                <use xlink:href="/images/sprite.svg#icon-search"></use>
+            </svg>
+            <input class="search__field" type="search" name="search" id="search" placeholder="Поиск фильмов">
+        </label>
+        <button type="button" class="btn search__btn" aria-label="Найти">
+            <svg width="24" height="24" class="header__form-icon" aria-hidden="true">
+                <use xlink:href="/images/sprite.svg#icon-search"></use>
+            </svg>
+        </button>
+    </form>
+</template>
+<script setup>
+</script>
